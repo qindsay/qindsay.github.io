58d130140a3509a7f79e882938d38f6599da2077
Lindsay Qin <lindsaymqin@gmail.com>
2025-05-25 13:55:25 -0700

    Updated resume link.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,7 +36,7 @@ const NavBar = () => {
          <ul>
            <li><Link to='/about'>About</Link></li>
            <li><Link to='/work'>Work</Link></li>
-           <li><a href="https://drive.google.com/file/d/1xrISeZxBYSH6D-zwtsyiakH7SjIXhXhC/view?usp=sharing" target="_blank" 
+           <li><a href="https://drive.google.com/file/d/1cJpv8MzmvGKhdeo1WSEcoT9-2Gf6YDZm/view?usp=sharing" target="_blank" 
                 rel="noopener noreferrer">Resume</a></li>
          </ul>
        </div>
@@ -48,7 +48,7 @@ const NavBar = () => {
           <ul>  
             <li><Link to='/about'>About</Link></li>
             <li><Link to='/work'>Work</Link></li>
-            <li><a href="https://drive.google.com/file/d/1xrISeZxBYSH6D-zwtsyiakH7SjIXhXhC/view?usp=sharing" target="_blank" 
+            <li><a href="https://drive.google.com/file/d/1cJpv8MzmvGKhdeo1WSEcoT9-2Gf6YDZm/view?usp=sharing" target="_blank" 
                   rel="noopener noreferrer">Resume</a></li>
           </ul>
     </div>
